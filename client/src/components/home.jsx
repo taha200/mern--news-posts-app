6b280d3b533d369803ab8de1e2b16b97a491cd2d
taha200 <41546947+taha200@users.noreply.github.com>
2018-11-17 16:37:19 +0500

    without proxy

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 
 export default class Home extends Component {
     constructor(props){
-        super(props)
+        super(props);
         this.state= {
             title:'',
             description:'',
-            link:''
+            link:'',
+            Posts:[]
         }
+        fetch('http://localhost:8000').then(res=>res.json()).then(data=>{
+    const Postsfetch = data
+  this.setState({
+    Posts:Postsfetch
+  })
+  console.log(this.state.Posts)
+  })
     }
     TextBoxVal(e){
         this.setState({
@@ -15,36 +23,89 @@ export default class Home extends Component {
         })
   
 }
+ componentWillMount(){
 
+   fetch('http://localhost:8000').then(res=>res.json()).then(data=>{
+    const Postsfetch = data
+  this.setState({
+    Posts:Postsfetch
+  })
+  console.log(this.state.Posts)
+  })
+}
     
  async  submitPost(e){
-        const userPost=this.state
-        console.log(userPost)
-      await fetch('/users',{
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userPost)
-          }).then(res=>{res.json()
-          console.log(res)
- }).then(data=>{
-            console.log(data)
-            if(data){
-                console.log(data)
-                this.setState({
-                    title:'',
-                    description:'',
-                    link:''
-                })
-              }
-              else
-              alert(data.message)
-          }).catch(err=>console.error(err));
+  e.preventDefault()
+  const userPost=this.state
+await fetch('http://localhost:8000/users',{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userPost)
+    }).then(res=>res.json()).then(data=>{
+      console.log(data)
+      if(data){
+          console.log(data)
+          const Postinsert = this.state.Posts
+          Postinsert.push(data)
+          this.setState({
+              title:'',
+              description:'',
+              link:'',
+              Posts:Postinsert
+          })
+        }
+        else
+        alert('error')
+    }).catch(err=>console.error(err));
     }
+    async Postdel(id){
+      await fetch('http://localhost:8000/users/remove/'+id, {
+          method: 'DELETE',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        }).then(res=>res.json()).then(data=>{
+        }).catch(err=>console.error(err));
+  }
 
   render() {
+
+    if(!this.state.Posts){
+      return null
+    }
+
+   const  Postrender=this.state.Posts.map(function(value){
+      return (
+//       <div className="col-md-12" style={{marginBottom:'5px'}}>
+      
+// <div className="card"> 
+// <div className="card-body">
+//   <h5 className="card-title">{value.title}</h5>
+//   <hr/>
+//   <p className="card-text">{value.description}
+//   </p>
+//   <hr/>
+// <a href={value.link}>Go to whole Scenario</a>
+// <hr/>
+// <button onClick={()=>this.Postdel(value._id)}>Del</button>
+
+// </div>
+// </div>
+//     </div>
+<div>
+
+  <li id={value._id}>{value.title} <button onClick={(e)=>{this.Postdel(e.target.id)
+  console.log(value._id)
+  }
+}>Del</button></li>
+</div>
+      )
+    })
+  
     return (
       <div>
         <label htmlFor="title">Title:</label><input type="text" name="title" value={this.state.title} onChange={(e)=>this.TextBoxVal(e)}/>
@@ -53,6 +114,7 @@ export default class Home extends Component {
          <br/>
        <label htmlFor="link">Link:</label><input type="text" name="link" value={this.state.link} onChange={(e)=>this.TextBoxVal(e)}/>
        <button onClick={this.submitPost.bind(this)}>Submit</button>
+       {Postrender}
       </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path')
 var mongojs = require('mongojs')
+var cors = require('cors')
 var db = mongojs('Posts', ['userPosts'])
 // create an express app
 var app = express();
 var bodyParser= require('body-parser')
+app.use(cors())
 app.use(bodyParser.json())  //Body Parser MiddleWare
 app.use(express.json())
 // create an express route for the home page
@@ -21,10 +23,26 @@ app.get('/', function(req, res) {
 });
 app.post('/users', function(req, res) {
     var userPost= req.body
-    console.log(userPost)
-    db.userPosts.insert(userPost)
-    res.json(userPost)
+    db.userPosts.insert(userPost,function (err, docs) {
+        if(err)
+        console.log(err)
+        else{
+       console.log(docs)
+       res.json(docs)
+        }
 });
+})
+app.delete('/users/remove:_id',function(req,res){
+    let id = req.params._id
+    db.userPosts.remove(id,function (err, docs) {
+        if(err)
+        console.log(err)
+        else{
+       console.log(docs)
+       res.json(docs)
+        }
+    })
+})
 // start the server on port 8080
 app.listen(8000);
 // send a message
